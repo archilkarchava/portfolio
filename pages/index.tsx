bee8fd014b243ed7086acf0cdade8bbdb4c94086
Archil Karchava <archil1997@gmail.com>
2020-10-30 10:07:10 +0500

    Make layout responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,11 +9,18 @@ export const Home: React.FC = () => {
       </Head>
       <div>
         <main>
-          <h1 className="text-6xl font-bold leading-tight">Archil Karchava</h1>
-          <h2 className="text-4xl font-semibold">Software engineer</h2>
+          <h1 className="text-3xl font-bold leading-tight sm:text-5xl md:text-6xl">
+            Archil Karchava
+          </h1>
+          <h2 className="text-xl font-semibold sm:text-3xl md:text-4xl">
+            Software engineer
+          </h2>
         </main>
         <footer className="flex items-center my-8">
-          <a href="mailto:hello@archil.dev" className="mr-4 text-2xl">
+          <a
+            href="mailto:hello@archil.dev"
+            className="mr-3 text-xl md:mr-4 md:text-2xl"
+          >
             hello@archil.dev
           </a>{' '}
           <a
@@ -21,7 +28,7 @@ export const Home: React.FC = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src="/github.svg" alt="Github Logo" className="h-7" />
+            <img src="/github.svg" alt="Github Logo" className="h-5 md:h-7" />
           </a>
         </footer>
       </div>
