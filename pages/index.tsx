cdd9c021c6353595613d32ad945375cb24467af5
Archil Karchava <archil1997@gmail.com>
2020-11-01 12:01:12 +0500

    feat: add "technologies I use" section

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,15 @@ export const Home: React.FC = () => {
           <h2 className="text-xl font-semibold sm:text-3xl md:text-4xl">
             Software engineer
           </h2>
-        </main>
-        <footer className="flex items-center my-8">
+          <div className="my-6 text-lg sm:text-xl md:text-2xl">
+            <h3 className="font-semibold">Technologies I use:</h3>
+            <p>
+              React • Redux • TypeScript • JavaScript • HTML • CSS • SASS •
+              Webpack • REST • GraphQL • Go • Node.js • PostgreSQL
+            </p>
+          </div>
+        </div>
+        <footer className="flex items-center">
           <a
             href="mailto:hello@archil.dev"
             className="mr-3 text-xl md:mr-4 md:text-2xl"
