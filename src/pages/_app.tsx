4b1b87b3d411848c711e5a23b00643cf2e519914
Archil Karchava <archil1997@gmail.com>
2020-12-13 11:21:41 +0500

    feat: enable experimental font optimization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,18 @@
+import '@styles/index.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import 'preact/debug'
-import 'styles/index.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+          rel="stylesheet"
+        />
         <meta name="theme-color" content="#ffffff" />
         <title>Archil Karchava</title>
       </Head>
