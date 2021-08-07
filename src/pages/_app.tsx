851712b9ff0cef6878a4ff93bccfe67136823e8d
Archil Karchava <hello@archil.dev>
2021-08-07 17:25:27 +0500

    fix: google font prefetching

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,13 +7,6 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
-          rel="stylesheet"
-        />
-        <meta name="theme-color" content="#ffffff" />
         <title>{FULL_NAME}</title>
       </Head>
       <main className="w-full h-full">
