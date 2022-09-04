b9bd55852ea707583805479703b595ed384c9ba1
Archil Karchava <hello@archil.dev>
2022-09-04 18:32:21 +0400

    feat: add prettier-plugin-tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -68,13 +68,13 @@ export function Home({ name, email, pinnedRepositories }: Props) {
       <Head>
         <title>{name}</title>
       </Head>
-      <main className="flex flex-col w-full h-full">
-        <div className="max-w-4xl p-5 m-auto">
+      <main className="flex h-full w-full flex-col">
+        <div className="m-auto max-w-4xl p-5">
           <div>
             <h1 className="text-4xl font-bold leading-tight sm:text-6xl">
               {name}
             </h1>
-            <div className="flex items-center mt-1 text-2xl sm:mt-3 sm:text-4xl">
+            <div className="mt-1 flex items-center text-2xl sm:mt-3 sm:text-4xl">
               <a href={`mailto:${email}`} className="mr-4">
                 {email}
               </a>
@@ -85,29 +85,29 @@ export function Home({ name, email, pinnedRepositories }: Props) {
                 rel="noopener noreferrer"
                 className="text-current hover:opacity-70"
               >
-                <GithubIcon className="w-6 h-6 sm:w-9 sm:h-9" />
+                <GithubIcon className="h-6 w-6 sm:h-9 sm:w-9" />
               </a>
             </div>
           </div>
           {pinnedRepositories.length > 0 && (
             <div className="mt-8">
               <h2 className="mb-2 text-2xl">Some of my projects:</h2>
-              <ol className="flex flex-wrap -m-2">
+              <ol className="-m-2 flex flex-wrap">
                 {pinnedRepositories.map((repo) => {
                   return (
                     <li
                       key={repo.id}
-                      className="flex-grow w-full p-4 m-2 border border-gray-300 rounded-lg md:w-5/12 dark:border-gray-700"
+                      className="m-2 w-full flex-grow rounded-lg border border-gray-300 p-4 dark:border-gray-700 md:w-5/12"
                     >
                       <div>
                         <div className="flex flex-row">
                           {repo.homepageUrl && (
-                            <div className="flex flex-col items-start mr-2 text-lg font-semibold">
+                            <div className="mr-2 flex flex-col items-start text-lg font-semibold">
                               <span>website: </span>
                               <span>code: </span>
                             </div>
                           )}
-                          <div className="flex flex-col flex-grow text-lg font-semibold">
+                          <div className="flex flex-grow flex-col text-lg font-semibold">
                             {repo.homepageUrl && (
                               <a
                                 href={repo.homepageUrl}
